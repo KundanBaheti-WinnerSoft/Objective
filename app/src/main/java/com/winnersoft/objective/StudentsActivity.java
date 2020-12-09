960308c9f17d742c370bb9af07b564c7bcb31bf0
KundanBaheti-WinnerSoft <kundan.baheti@winnersoft.co.in>
2020-12-09 17:45:44 +0530

    show total count of reg. students

--- app/src/main/java/com/winnersoft/objective/StudentsActivity.java
+++ app/src/main/java/com/winnersoft/objective/StudentsActivity.java
@@ -35,7 +35,7 @@ import java.util.Map;
 
 import fr.ganfra.materialspinner.MaterialSpinner;
 
-public class StudentsActivity extends AppCompatActivity implements ItemClickListener{
+public class StudentsActivity extends AppCompatActivity implements ItemClickListener {
     String SecurityToken, userName, userRole, userId;
     private SharedPreferences sharedPreferences;
     CommonCode commonCode = new CommonCode();
@@ -53,6 +53,8 @@ public class StudentsActivity extends AppCompatActivity implements ItemClickList
     TableRow trRegCount;
     int courseId = 0;
     String courseName = "";
+    String count = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,11 +69,11 @@ public class StudentsActivity extends AppCompatActivity implements ItemClickList
         userName = sharedPreferences.getString("username", "");
         userId = sharedPreferences.getString("userId", "");
 
-        tvRegStudCount=findViewById(R.id.tv_reg_stud_count);
-        trRegCount=findViewById(R.id.tr_reg_count);
-        cvTotalStudentsCount=findViewById(R.id.cv_total_students_count);
-        rvStudentList=findViewById(R.id.rv_studentList);
-        spnCourse=findViewById(R.id.spn_course);
+        tvRegStudCount = findViewById(R.id.tv_total_students_count);
+        trRegCount = findViewById(R.id.tr_reg_count);
+        cvTotalStudentsCount = findViewById(R.id.cv_total_students_count);
+        rvStudentList = findViewById(R.id.rv_studentList);
+        spnCourse = findViewById(R.id.spn_course);
         getExamCotrollerCourseList();
     }
 
@@ -80,104 +82,6 @@ public class StudentsActivity extends AppCompatActivity implements ItemClickList
         super.onRestart();
         getExamCotrollerCourseList();
     }
-    private void getCoursewiseStudentData(){
-        jsonurl = urllink.url + "studentCourse/getStudentsByCourse/"+courseId;
-        arraymap.clear();
-        if (commonCode.checkConnection(getApplicationContext())) {
-            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
-            progressDialog = new ProgressDialog(StudentsActivity.this);
-            progressDialog.setMessage(getResources().getString(R.string.loading));
-            progressDialog.show();
-            progressDialog.setCancelable(false);
-
-            JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, jsonurl, null, new Response.Listener<JSONArray>() {
-                @Override
-                public void onResponse(JSONArray response) {
-//[{"id":176,"otp":null,"loginName":"pk557@gmail.com","language":null,"userType":{"userTypeName":"student","id":4},"softDelete":false,"roles":[{"id":202,"roleDescription":null,"roleName":null}],"userRoles":null,"first_name":"prajkta","middle_name":"b","last_name":"kole","m_name":"m","birthdate":"1111-11-11","admissionDate":"1911-11-11","email":"pk557@gmail.com","mobile":"8787978721","bloodGroup":"O+ve","gender":"Female","emergencyMobNo":"8978709098","address":"pune","highestQualification":"BE","nationality":"india","state":"maharashtra","city":"pune","organization":"id":2,"version":null,"createdDate":null,"createdBy":null,"modifiedDate":null,"modifiedBy":null,"orgOwnner":"Shrutanjay","orgStartedDate":1577817000000,"orgName":"demo","orgRegId":"REE124","orgAddress":"Baner,pune","orgContactNo":"8975730288","softDelete":false},"passwdString":"prajkta0818#"}]
-                    arraymap.clear();
-                    progressDialog.dismiss();
-                    if (response.length() > 0) {
-                        for (int i = 0; i < response.length(); i++) {
-                            try {
-                                HashMap<String, String> hashMap = new HashMap<>();
-                                JSONObject object = response.getJSONObject(i);
-                                hashMap.put("id", object.getString("id"));
-                                hashMap.put("first_name", object.getString("first_name"));
-                                hashMap.put("middle_name", object.getString("middle_name"));
-                                hashMap.put("last_name", object.getString("last_name"));
-                                hashMap.put("m_name", object.getString("m_name"));
-                                hashMap.put("birthdate", object.getString("birthdate"));
-                                hashMap.put("admissionDate", object.getString("admissionDate"));
-                                hashMap.put("gender", object.getString("gender"));
-                                hashMap.put("email", object.getString("email"));
-                                hashMap.put("mobile", object.getString("mobile"));
-                                hashMap.put("bloodGroup", object.getString("bloodGroup"));
-                                hashMap.put("emergencyMobNo", object.getString("emergencyMobNo"));
-                                hashMap.put("highestQualification", object.getString("highestQualification"));
-
-                                hashMap.put("nationality", object.getString("nationality"));
-                                hashMap.put("state", object.getString("state"));
-                                hashMap.put("city", object.getString("city"));
-                                hashMap.put("address", object.getString("address"));
-
-                                JSONObject userObj = object.getJSONObject("userType");
-                                hashMap.put("userId", userObj.getString("id"));
-                                hashMap.put("userTypeName", userObj.getString("userTypeName"));
-
-                                hashMap.put("address", object.getString("address"));
-                                hashMap.put("softDelete", String.valueOf(object.getBoolean("softDelete")));
-
-
-                                arraymap.add(hashMap);
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    } else {
-                        commonCode.AlertDialog_Pbtn(StudentsActivity.this, getResources().getString(R.string.notFound), getResources().getString(R.string.examControllerNotFound), getResources().getString(R.string.ok));
-                    }
-                    showdata(arraymap);
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    progressDialog.dismiss();
-                    String err = error.toString();
-                    if (err.equals("com.android.volley.AuthFailureError")) {
-                        Toast.makeText(getApplicationContext(), R.string.tokenExpire, Toast.LENGTH_LONG).show();
-
-                        SharedPreferences sp = getSharedPreferences("login", MODE_PRIVATE);
-                        sp.edit().clear().commit();
-                        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                        startActivity(intent);
-                    } else {
-                        Toast.makeText(getApplicationContext(), R.string.serverError, Toast.LENGTH_LONG).show();
-                    }
-                }
-            })
-                    //This is for Headers If You Needed
-            {
-                @Override
-                public Map<String, String> getHeaders() throws AuthFailureError {
-                    Map<String, String> params = new HashMap<String, String>();
-                    params.put("x-Auth-token", SecurityToken);
-                    return params;
-                }
-            };
-            requestQueue.add(jsonArrayRequest);
-        } else {
-            commonCode.AlertDialog_Pbtn(StudentsActivity.this, getResources().getString(R.string.noInternetConnection), getResources().getString(R.string.plsConnectToInternet), getResources().getString(R.string.ok));
-        }
-    }
-     private void showdata(ArrayList<HashMap<String, String>> arraymap) {
-        madapter = new ExamControllerAdapter(getApplicationContext(), arraymap);
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
-        rvStudentList.setLayoutManager(mLayoutManager);
-         rvStudentList.setAdapter(madapter);
-         rvStudentList.setVisibility(View.VISIBLE);
-        madapter.setClickListener(this);
-    }
-
     private void getExamCotrollerCourseList() {
         arraymapCourse.clear();
         nameListCourse.clear();
@@ -204,6 +108,14 @@ public class StudentsActivity extends AppCompatActivity implements ItemClickList
                     if (response.length() > 0) {
 
                         try {
+
+
+                            count = response.getString("studentCount");
+                            tvRegStudCount.setText(count);
+
+//                            or
+//                            tvRegStudCount.setText(response.getString("studentCount"));
+
                             JSONArray jsonArray = response.getJSONArray("courses");
                             for (int i = 0; i < jsonArray.length(); i++) {
                                 JSONObject obj = jsonArray.getJSONObject(i);
@@ -232,12 +144,13 @@ public class StudentsActivity extends AppCompatActivity implements ItemClickList
                                     getCoursewiseStudentData();
                                     trRegCount.setVisibility(View.VISIBLE);
 
-
                                 } else {
                                     courseId = 0;
                                     courseName = "";
                                     spnCourse.setSelection(0);
                                     trRegCount.setVisibility(View.GONE);
+                                    arraymap.clear();
+
 
                                 }
 
@@ -286,6 +199,109 @@ public class StudentsActivity extends AppCompatActivity implements ItemClickList
             commonCode.AlertDialog_Pbtn(StudentsActivity.this, getResources().getString(R.string.noInternetConnection), getResources().getString(R.string.plsConnectToInternet), getResources().getString(R.string.ok));
         }
     }
+
+    private void getCoursewiseStudentData() {
+
+            jsonurl = urllink.url + "studentCourse/getStudentsByCourse/" + courseId;
+            arraymap.clear();
+            if (commonCode.checkConnection(getApplicationContext())) {
+                RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+                progressDialog = new ProgressDialog(StudentsActivity.this);
+                progressDialog.setMessage(getResources().getString(R.string.loading));
+                progressDialog.show();
+                progressDialog.setCancelable(false);
+
+                JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, jsonurl, null, new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+//[{"id":176,"otp":null,"loginName":"pk557@gmail.com","language":null,"userType":{"userTypeName":"student","id":4},"softDelete":false,"roles":[{"id":202,"roleDescription":null,"roleName":null}],"userRoles":null,"first_name":"prajkta","middle_name":"b","last_name":"kole","m_name":"m","birthdate":"1111-11-11","admissionDate":"1911-11-11","email":"pk557@gmail.com","mobile":"8787978721","bloodGroup":"O+ve","gender":"Female","emergencyMobNo":"8978709098","address":"pune","highestQualification":"BE","nationality":"india","state":"maharashtra","city":"pune","organization":"id":2,"version":null,"createdDate":null,"createdBy":null,"modifiedDate":null,"modifiedBy":null,"orgOwnner":"Shrutanjay","orgStartedDate":1577817000000,"orgName":"demo","orgRegId":"REE124","orgAddress":"Baner,pune","orgContactNo":"8975730288","softDelete":false},"passwdString":"prajkta0818#"}]
+                        arraymap.clear();
+                        progressDialog.dismiss();
+                        if (response.length() > 0) {
+
+                            for (int i = 0; i < response.length(); i++) {
+                                try {
+                                    HashMap<String, String> hashMap = new HashMap<>();
+                                    JSONObject object = response.getJSONObject(i);
+                                    hashMap.put("id", object.getString("id"));
+                                    hashMap.put("first_name", object.getString("first_name"));
+                                    hashMap.put("middle_name", object.getString("middle_name"));
+                                    hashMap.put("last_name", object.getString("last_name"));
+                                    hashMap.put("m_name", object.getString("m_name"));
+                                    hashMap.put("birthdate", object.getString("birthdate"));
+                                    hashMap.put("admissionDate", object.getString("admissionDate"));
+                                    hashMap.put("gender", object.getString("gender"));
+                                    hashMap.put("email", object.getString("email"));
+                                    hashMap.put("mobile", object.getString("mobile"));
+                                    hashMap.put("bloodGroup", object.getString("bloodGroup"));
+                                    hashMap.put("emergencyMobNo", object.getString("emergencyMobNo"));
+                                    hashMap.put("highestQualification", object.getString("highestQualification"));
+
+                                    hashMap.put("nationality", object.getString("nationality"));
+                                    hashMap.put("state", object.getString("state"));
+                                    hashMap.put("city", object.getString("city"));
+                                    hashMap.put("address", object.getString("address"));
+
+                                    JSONObject userObj = object.getJSONObject("userType");
+                                    hashMap.put("userId", userObj.getString("id"));
+                                    hashMap.put("userTypeName", userObj.getString("userTypeName"));
+
+                                    hashMap.put("address", object.getString("address"));
+                                    hashMap.put("softDelete", String.valueOf(object.getBoolean("softDelete")));
+
+
+                                    arraymap.add(hashMap);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        } else {
+                            commonCode.AlertDialog_Pbtn(StudentsActivity.this, getResources().getString(R.string.notFound), getResources().getString(R.string.examControllerNotFound), getResources().getString(R.string.ok));
+                        }
+                        showdata(arraymap);
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progressDialog.dismiss();
+                        String err = error.toString();
+                        if (err.equals("com.android.volley.AuthFailureError")) {
+                            Toast.makeText(getApplicationContext(), R.string.tokenExpire, Toast.LENGTH_LONG).show();
+
+                            SharedPreferences sp = getSharedPreferences("login", MODE_PRIVATE);
+                            sp.edit().clear().commit();
+                            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(getApplicationContext(), R.string.serverError, Toast.LENGTH_LONG).show();
+                        }
+                    }
+                })
+                        //This is for Headers If You Needed
+                {
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<String, String>();
+                        params.put("x-Auth-token", SecurityToken);
+                        return params;
+                    }
+                };
+                requestQueue.add(jsonArrayRequest);
+            } else {
+                commonCode.AlertDialog_Pbtn(StudentsActivity.this, getResources().getString(R.string.noInternetConnection), getResources().getString(R.string.plsConnectToInternet), getResources().getString(R.string.ok));
+            }
+
+    }
+
+    private void showdata(ArrayList<HashMap<String, String>> arraymap) {
+        madapter = new ExamControllerAdapter(getApplicationContext(), arraymap);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        rvStudentList.setLayoutManager(mLayoutManager);
+        rvStudentList.setAdapter(madapter);
+        rvStudentList.setVisibility(View.VISIBLE);
+        madapter.setClickListener(this);
+    }
+
 
     @Override
     public void onClick(View view, int position) {
